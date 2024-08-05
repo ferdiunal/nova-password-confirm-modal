28709e5dd1fd448793cc0de22f840a970ded141c
Ferdi ÜNAL <ferdiunal@outlook.com>
2024-08-05 22:33:31 +0300

    chore: Update FieldServiceProvider with translations handling

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Ferdiunal\NovaPasswordConfirmModal;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Nova\Events\ServingNova;
@@ -17,14 +18,25 @@ class FieldServiceProvider extends ServiceProvider
     public function boot()
     {
         Nova::serving(function (ServingNova $event) {
-            Nova::script('nova-password-confirm-modal', __DIR__.'/../dist/js/field.js');
-            Nova::style('nova-password-confirm-modal', __DIR__.'/../dist/css/field.css');
-            Nova::translations(realpath(__DIR__.sprintf('/../lang/%s.json', $this->app->getLocale())));
+            $this->translations();
+            Nova::script('nova-password-confirm-modal', __DIR__ . '/../dist/js/field.js');
+            Nova::style('nova-password-confirm-modal', __DIR__ . '/../dist/css/field.css');
         });
 
         Route::middleware(['nova'])
             ->prefix('nova-vendor/ferdiunal/nova-password-confirm-modal')
-            ->group(__DIR__.'/../routes/api.php');
+            ->group(__DIR__ . '/../routes/api.php');
+    }
+
+
+    private function translations()
+    {
+        $translate = realpath(__DIR__ . sprintf('/../lang/%s.json', $this->app->getLocale()));
+        if (File::exists($translate)) {
+            Nova::translations($translate);
+        } else {
+            Nova::translations(realpath(__DIR__ . '/../lang/en.json'));
+        }
     }
 
     /**
